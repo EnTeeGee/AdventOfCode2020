35a58896875102e2e333495466352ced8c169b59
EnTeeGee <enteegee@protonmail.com>
2020-12-11 20:05:50 +0000

    Solutions for day 11 and common point class

---
+++ AdventOfCode2020/Common/Point.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Common
+{
+    struct Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int ManDistanceTo(Point to)
+        {
+            return Math.Abs(X - to.X) + Math.Abs(Y - to.Y);
+        }
+
+        public Point[] GetSurrounding4()
+        {
+            return new Point[]
+            {
+                new Point(X, Y - 1),
+                new Point(X + 1, Y),
+                new Point(X, Y + 1),
+                new Point(X - 1, Y)
+            };
+        }
+
+        public Point[] GetSurrounding8()
+        {
+            return new Point[]
+            {
+                new Point(X, Y - 1),
+                new Point(X + 1, Y - 1),
+                new Point(X + 1, Y),
+                new Point(X + 1, Y + 1),
+                new Point(X, Y + 1),
+                new Point(X - 1, Y + 1),
+                new Point(X - 1, Y),
+                new Point(X - 1, Y - 1)
+            };
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point))
+                return false;
+
+            return ((Point)obj).X == X && ((Point)obj).Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X | (Y << 8);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ AdventOfCode2020/Solutions/Day11.cs
@@ -0,0 +1,160 @@
+﻿using AdventOfCode2020.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Solutions
+{
+    class Day11
+    {
+        [Solution(11, 1)]
+        public int Solution1(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+            var seats = new List<Point>();
+
+            for(var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                for(var j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == 'L')
+                        seats.Add(new Point(j, i));
+                }
+            }
+
+            var hasChanged = true;
+            var takenSeats = new HashSet<Point>();
+
+            while (hasChanged)
+            {
+                var newTakenSeats = new HashSet<Point>();
+                hasChanged = false;
+
+                foreach(var item in seats)
+                {
+                    var surrounding = item.GetSurrounding8().Where(it => takenSeats.Contains(it)).Count();
+                    var taken = takenSeats.Contains(item);
+                    var newState = taken;
+
+                    if(taken && surrounding >= 4)
+                    {
+                        hasChanged = true;
+                        newState = false;
+                    }
+                    else if(!taken && surrounding == 0)
+                    {
+                        hasChanged = true;
+                        newState = true;
+                    }
+
+                    if (newState)
+                        newTakenSeats.Add(item);
+                }
+
+                takenSeats = newTakenSeats;
+            }
+
+            return takenSeats.Count();
+        }
+
+        [Solution(11, 2)]
+        public int Solution2(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+            var seats = new List<Point>();
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                for (var j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == 'L')
+                        seats.Add(new Point(j, i));
+                }
+            }
+
+            var hasChanged = true;
+            var takenSeats = new HashSet<Point>();
+            var surroundingSeats = GenerateSurrounding(seats);
+
+            while (hasChanged)
+            {
+                var newTakenSeats = new HashSet<Point>();
+                hasChanged = false;
+
+                foreach (var item in seats)
+                {
+                    var surrounding = surroundingSeats[item].Where(it => takenSeats.Contains(it)).Count();
+                    var taken = takenSeats.Contains(item);
+                    var newState = taken;
+
+                    if (taken && surrounding >= 5)
+                    {
+                        hasChanged = true;
+                        newState = false;
+                    }
+                    else if (!taken && surrounding == 0)
+                    {
+                        hasChanged = true;
+                        newState = true;
+                    }
+
+                    if (newState)
+                        newTakenSeats.Add(item);
+                }
+
+                takenSeats = newTakenSeats;
+            }
+
+            return takenSeats.Count();
+        }
+
+        private Dictionary<Point, Point[]> GenerateSurrounding(List<Point> points)
+        {
+            var bounding = new Point(points.Max(it => it.X), points.Max(it => it.Y));
+            var set = new HashSet<Point>(points);
+
+            var output = new Dictionary<Point, Point[]>();
+
+            foreach(var item in points)
+                output.Add(item, GetSurroundingForPoint(item, set, bounding));
+
+            return output;
+        }
+
+        private Point[] GetSurroundingForPoint(Point point, HashSet<Point> points, Point bounding)
+        {
+            var steppers = new List<Func<Point, Point>>{
+                it => new Point(it.X, it.Y - 1),
+                it => new Point(it.X + 1, it.Y - 1),
+                it => new Point(it.X + 1, it.Y),
+                it => new Point(it.X + 1, it.Y + 1),
+                it => new Point(it.X, it.Y + 1),
+                it => new Point(it.X - 1, it.Y + 1),
+                it => new Point(it.X - 1, it.Y),
+                it => new Point(it.X - 1, it.Y - 1)
+            };
+
+            var output = new List<Point>();
+            foreach(var item in steppers)
+            {
+                var next = point;
+
+                while(next.X >= 0 && next.X <= bounding.X && next.Y >= 0 && next.Y <= bounding.Y)
+                {
+                    next = item.Invoke(next);
+                    if (points.Contains(next))
+                    {
+                        output.Add(next);
+                        break;
+                    }
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
